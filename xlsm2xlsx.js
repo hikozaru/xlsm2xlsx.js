c2ac44cad27d5c64fb1178165fab8bfe2d12850a
hikozaru <takashi@hikozaru.com>
2022-08-28 13:19:56 +0900

    Add files via upload

---
+++ xlsm2xlsx.js
@@ -0,0 +1,25 @@
+// https://docs.microsoft.com/ja-jp/office/vba/api/excel.xlfileformat
+var xlWorkbookDefault = 51;
+
+var ExcelApp = new ActiveXObject( "Excel.Application" );
+
+// *.jsファイルにDrag&Dropされたファイルを順番に処理
+var args = WScript.Arguments;
+for (var i = 0; i < args.length; i++ ) {
+  var orgFilename = args(i);
+
+  // 特定の拡張子のみ処理を実施
+  var orgExtension = orgFilename.split('.').pop();
+  if( orgExtension == 'xlsm' ){
+    var baseName = orgFilename.substring(0, orgFilename.indexOf('.'+orgExtension))
+
+    // ファイルを開いて形式を変更して保存
+    var book = ExcelApp.Workbooks.Open( orgFilename );
+    ExcelApp.DisplayAlerts= false;
+    book.SaveAs( baseName + '.xlsx' ,  xlWorkbookDefault );
+    ExcelApp.DisplayAlerts= true;
+    ExcelApp.Quit();
+  }
+}
+WScript.Echo( "finish" );
+ExcelApp = null;
